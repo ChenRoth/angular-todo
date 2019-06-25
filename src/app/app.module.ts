ead2f01dee4d001b28e1cc4311c051e4d6f019b2
Chen Roth <rothchen@gmail.com>
2019-06-25 21:13:29 +0300

    add the required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTaskComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
