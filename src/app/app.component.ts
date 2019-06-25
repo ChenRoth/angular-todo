83469f93ac961ae41cd92b1598338443811985d2
Chen Roth <rothchen@gmail.com>
2019-06-25 21:18:51 +0300

    add Task data structure and a tasks field to App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+    tasks: Task[] = [];
 }
